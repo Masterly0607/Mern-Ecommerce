6c3fc074c3fb9318d2bb30e7ba13a600efa90397
Sok Masterly <sokmasterlychanon06@gmail.com>
2025-12-30 18:34:23 +0700

    Backend setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use("/api/auth", authRoutes); // use for grouping routes
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+});
+
+// Note
+// Note1: app.use(express.json()); // “Hey Express,  parse JSON request bodies and put the result in req.body.” parse = Parse = convert raw text into a structured format the program can understand and use. Parse JSON = turn a JSON text (string) into a real JavaScript object.
+// Explain in detail:
+// 1. HTTP body arrives as a stream, chunk by chunk: chunk 1 → "{", chunk 2 → ""email":"a@gmail.com"", chunk 3 → "}"
+// 2. express.json() = Behind the scenes: Reads all incoming chunks, Combines them into one string, Runs JSON.parse(), Assigns result to req.body.
