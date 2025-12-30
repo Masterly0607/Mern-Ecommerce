c0c9a1451dfe04d3938201e736429188191486d0
Sok Masterly <sokmasterlychanon06@gmail.com>
2025-12-31 01:39:56 +0700

    Added product routes

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -7,5 +7,5 @@ export const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
   tls: {}, // important for rediss:// (Upstash)
 });
 
-redis.on("connect", () => console.log("✅ Redis connected"));
+redis.on("connect", () => console.log("Redis is connected"));
 redis.on("error", (err) => console.log("❌ Redis error:", err));
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes); // use for grouping routes
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
